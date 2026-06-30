e0e3795c2fc3dab164c8f5102028829c248e7b50
viki <ravikishevi2@gmail.com>
2026-06-30 12:02:48 +0700

    proyek pertama : Membuat Fungsi hitung diskon

---
+++ app.js
@@ -0,0 +1,8 @@
+function hitunganDiskon(harga, diskon) {
+    let hargaDiskon = harga - diskon ;
+    return hargaDiskon;
+}
+
+let HargaGamepass = hitunganDiskon(100000, 20000);
+
+console.log(`harga Gamepass setelah diskon adalah ${HargaGamepass}`);

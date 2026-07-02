164092de3a87e0b3b18c34cd829ec9211e4d58f4
viki <ravikishevi2@gmail.com>
2026-07-02 10:52:35 +0700

    materi 3 SQL

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(`./kafe.db`);
 
-db.serialize(() => {
-    db.run(`
-        CREATE TABLE IF NOT EXISTS menu (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nama_makanan TEXT,
-            harga INTEGER)
-            `);
+//READ
+function lihatMenu() {
+    const query = `SELECT * FROM menu`;
+    db.all(query, [], (err, rows)=>{
+        if (err) {
+            console.error(`Gagal menampilkan menu`, err.message);
+            return;
+        }
+        console.log(`\n--- MENU KAFE ---`);
+        console.table(rows);
+    });
+}
 
-            console.log('Database and table created successfully.');
+//UPDATE
+function updateHarga(id, hargaBaru) {
+    const query = "UPDATE menu SET harga = ? WHERE id = ?";
+    db.run(query, [hargaBaru, id], function(err) {
+        if (err) {
+            console.error(`Gagal mengupdate harga menu`, err.message);
+            return;
+        }
+        console.log(`\n Berhasil memperbarui data`);
+        lihatMenu();
+    });
+}
+
+//DELETE
+function hapusMenu(id) {
+    const query = "DELETE FROM menu WHERE id = ?";
+    db.run(query, [id], function(err) {
+        if (err) {
+            console.error(`Gagal menghapus menu`, err.message);
+            return;
+        }
+        console.log(`\n Berhasil menghapus data`);
+        lihatMenu();
+    });
+}
+
+db.serialize(() => {
+    hapusMenu(3);
 });
-db.close();
